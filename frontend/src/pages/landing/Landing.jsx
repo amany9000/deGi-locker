fa952675230296147c987e0c5900ac4fcecd3663
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2023-02-06 22:05:14 +0530

    X

--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -17,7 +17,6 @@ const Landing = () => {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{" "}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
@@ -26,12 +25,10 @@ const Landing = () => {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
+              DeGi Locker
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              A ZKP-based credential store and KYC system on FVM and IPFS
             </p>
             {!Boolean(address) && (
               <div className="mt-10 flex items-center justify-center gap-x-6">
--- frontend/src/pages/verifierlanding/VerifierLanding.jsx
+++ frontend/src/pages/verifierlanding/VerifierLanding.jsx
@@ -61,13 +61,11 @@ const VerifierLanding = () => {
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
         <div className="mx-auto max-w-3xl text-center">
           <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
-            Understand User Flow.
-            <span className="sm:block"> Increase Conversion. </span>
+            WAGMI Verifier App
           </h1>
 
           <p className="mx-auto mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-            illo tenetur fuga ducimus numquam ea!
+            Please connect your wallet and verify your identity.
           </p>
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
